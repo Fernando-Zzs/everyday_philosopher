361b97ce4dca4ffc61039c97c2634f5a4e1272e6
fernando <760156389@qq.com>
2021-05-22 11:40:17 +0800

    整合1.0

---
+++ miniprogram/pages/personal/analyse/analyse.js
@@ -0,0 +1,73 @@
+// pages/analyse/analyse.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    analysis:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    if(this.data.analysis.length>0){
+    for(var i=0;i<this.data.analysis.length;i++){
+      this.animate('.i'+(i+1),
+      [
+        {transform:'scale('+this.data.analysis[i]+')'}
+      ])
+    }
+  }
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
